8981f36f1f4294d16e88da2675cc87d593d723d5
Krisztian <krisztian.valyi1@gmail.com>
2023-01-17 10:07:43 +0000

    fixed addEntry

--- src/SecretDiary.js
+++ src/SecretDiary.js
@@ -1,47 +1,43 @@
-class SecretDiary{
-    
-// - lock()
-//   - unlock()
-//   - addEntry()
-//   - getEntries()
-constructor(newEntries){
+class SecretDiary {
+  //   - lock()
+  //   - unlock()
+  //   - addEntry()
+  //   - getEntries()
+  constructor(newEntries) {
     this.newEntries = newEntries;
-
-
-}
-
-// entries=[];
-
-isLocked = true;
-    lock() {
-      this.isLocked = true;
-  }
-    unlock() {
-        this.isLocked = false;
   }
 
+  // entries=[];
 
-addEntry(str){
-    // if(this.isLocked===true){
-    //     throw new Error("Diary is locked!");
-    // }
-    // try{
-        this.newEntries.addEntry(str);
-        return this.newEntries.getEntries();
-    // }catch(e){
-    //     console.log(e);
-    // }
-}
+  isLocked = true;
+  lock() {
+    this.isLocked = true;
+  }
+  unlock() {
+    this.isLocked = false;
+  }
 
-// getEntries(){
-//     if(this.isLocked===true){
-//         throw new Error("Diary is locked!");
-//     }
-//     try{
-//         console.log(this.entries);
-//     }catch(e){
-//         console.log(e);
-//     }
-// }
+  addEntry(str) {
+    if (this.isLocked === true) {
+      throw new Error("Diary is locked!");
+    }
+    try {
+      this.newEntries.addEntry(str);
+      return this.newEntries.getEntries();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  // getEntries(){
+  //     if(this.isLocked===true){
+  //         throw new Error("Diary is locked!");
+  //     }
+  //     try{
+  //         console.log(this.entries);
+  //     }catch(e){
+  //         console.log(e);
+  //     }
+  // }
 }
 module.exports = SecretDiary;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-const SecretDiary = require('./SecretDiary');
-const Entries = require('./Entries');
-const secretDiery = new SecretDiary(new Entries('Diary 1'));
+const SecretDiary = require("./SecretDiary");
+const Entries = require("./Entries");
+const secretDiery = new SecretDiary(new Entries("Diary 1"));
 // secretDiery.addEntry('diary');
 
-
- console.log(secretDiery.isLocked);
+console.log(secretDiery.isLocked);
 secretDiery.unlock();
 console.log(secretDiery.isLocked);
-secretDiery.addEntry('string');
+secretDiery.addEntry("string");
 // secretDiery.getEntries();
